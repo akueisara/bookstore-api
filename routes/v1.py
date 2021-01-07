9698bbfdb08a78fb4b8d794a27ae9d37f62b744a
Kuei-Jung Hu <akueisara39@gmail.com>
2021-01-07 19:43:53 +0900

    5.6. Authentication

--- routes/v1.py
+++ routes/v1.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, Body, Header, File
+from fastapi import FastAPI, Body, Header, File, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from starlette.responses import Response
-from starlette.status import HTTP_201_CREATED
+from starlette.status import HTTP_201_CREATED, HTTP_401_UNAUTHORIZED
 
+from models.jwt_user import JWTUser
 from models.user import User
 from models.author import Author
 from models.book import Book
+from utils.secuirty import authenticate_user, create_jwt_token, check_jwt_token
 
 app_v1 = FastAPI(openapi_prefix="/v1")
 
@@ -12,7 +15,7 @@ app_v1 = FastAPI(openapi_prefix="/v1")
 # Save bookstore admin to db
 @app_v1.post("/user", status_code=HTTP_201_CREATED)
 # async def post_user(user: User, x_custom: str = Header(...)):
-async def post_user(user: User, x_custom: str = Header("default")):
+async def post_user(user: User, x_custom: str = Header("default"), jwt: bool = Depends(check_jwt_token)):
     return {
         "request_body": user,
         "custom_header": x_custom
@@ -78,3 +81,16 @@ async def upload_user_photo(response: Response, profile_photo: bytes = File(...)
     return {
         "file_size": str(len(profile_photo))
     }
+
+
+@app_v1.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    jwt_user_dict = {"username": form_data.username, "password": form_data.password}
+    jwt_user = JWTUser(**jwt_user_dict)
+    user = authenticate_user(jwt_user)
+
+    if user is None:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
+
+    jwt_token = create_jwt_token(user)
+    return {"token": jwt_token}
---
+++ utils/secuirty.py
@@ -0,0 +1,79 @@
+import time
+from datetime import datetime, timedelta
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+from models.jwt_user import JWTUser
+from utils.const import JWT_EXPIRATION_TIME_MINUTES, JWT_ALGORITHM, JWT_SECRET_KEY
+
+import jwt
+
+pwd_context = CryptContext(schemes=["bcrypt"])
+oauth_schema = OAuth2PasswordBearer(tokenUrl="/token")
+
+jwt_user_1 = {"username": "user1", "password": "$2b$12$os2gA.EYjJRNdZfx5iHN7em5YZlM2QdRqmKrmPDz.ItAT/.YsOPhW", "disable": False, "role": "admin"}
+fake_jwt_user_1 = JWTUser(**jwt_user_1)
+
+
+def get_hashed_password(password):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    try:
+        return pwd_context.verify(plain_password, hashed_password)
+    except Exception as e:
+        return False
+
+
+hashed = "$2b$12$os2gA.EYjJRNdZfx5iHN7em5YZlM2QdRqmKrmPDz.ItAT/.YsOPhW"
+print(verify_password("pass1", hashed))
+print(get_hashed_password("pass1"))
+
+
+# Authenticate username and password to give JWT
+def authenticate_user(user: JWTUser):
+    if fake_jwt_user_1.username == user.username:
+        if verify_password(user.password, fake_jwt_user_1.password):
+            user.role = "admin"
+            return user
+    return None
+
+
+# Create access JWT
+def create_jwt_token(user: JWTUser):
+    expiration = datetime.utcnow() + timedelta(minutes=JWT_EXPIRATION_TIME_MINUTES)
+    jwt_payload = {
+        "sub": user.username,
+        "role": user.role,
+        "exp": expiration
+    }
+    jwt_token = jwt.encode(jwt_payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+    return jwt_token
+
+
+# Create whether JWT token is correct
+def check_jwt_token(token: str = Depends(oauth_schema)):
+    try:
+        jwt_payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=JWT_ALGORITHM)
+        username = jwt_payload.get("sub")
+        role = jwt_payload.get("role")
+        expiration = jwt_payload.get("exp")
+        if time.time() < expiration:
+            if fake_jwt_user_1.username == username:
+                return final_checks(role)
+    except Exception as e:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
+
+    raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
+
+
+# Last checking and returning the final result
+def final_checks(role: str):
+    if role == "admin":
+        return True
+    else:
+        return False
